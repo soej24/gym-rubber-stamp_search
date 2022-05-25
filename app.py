f22e2f32273d076b2a907bfcd2f13d6d35574869
soej24 <soej24@naver.com>
2022-05-26 00:39:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-
-with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-    selected
 
 def main() :
     st.title('우리집에서 가까운 체육관 찾기')
 
-    menu = ['Home', '국내 체육관','종목별 체육관', '연령별 운동추천', 'EDA', 'ML']
-
+    menu = ['Home', '국내 체육관','종목별 체육관', '연령별 운동추천']
+    menu2 = ['EDA', 'ML']
     st.sidebar.selectbox('', menu)
+    st.sidebar.selectbox('', menu2)
 
+    input = st.text_input("message")
+    st.write(input)
 
 if __name__ == '__main__' :
     main()
