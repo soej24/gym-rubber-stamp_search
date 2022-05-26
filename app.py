19df1c9a463b887a1f763dcab8ff93dd886131e4
soej24 <soej24@naver.com>
2022-05-26 10:32:53 +0900

    add

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import streamlit as st
+from PIL import Image
 
 def main() :
+    image = Image.open('data/logo.png')
+    st.sidebar.image(image)  
+    
     st.title('우리집에서 가까운 체육관 찾기')
 
     menu = ['Home', '국내 체육관','종목별 체육관', '연령별 운동추천']
     menu2 = ['EDA', 'ML']
-    st.sidebar.selectbox('', menu)
-    st.sidebar.selectbox('', menu2)
 
-    input = st.text_input("message")
-    st.write(input)
+
+
+    
+    st.sidebar.selectbox('main_menu', menu)
+    st.sidebar.selectbox('EDA', menu2)
+
 
 if __name__ == '__main__' :
     main()
