7fc6f7c8f2f88abd26092262e6454e6e3e943df6
soej24 <soej24@naver.com>
2022-05-27 07:40:17 +0900

    add

--- app_exercise_recommendation.py
+++ app_exercise_recommendation.py
@@ -6,7 +6,7 @@ from PIL import Image
 def run_exercise_recommendationt() :
     st.title('연령별 운동 추천')
     # st.markdown("***") #라인줄
-    image = Image.open('data/sub_main_img.jpg')
+    image = Image.open('data/sub_main04_img.jpg')
     st.image(image, width=None, use_column_width=True)  
     st.markdown("***")
     df = pd.read_csv('data/gym_suggestion_ch.csv')
