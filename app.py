38ebf2a129ca770c4648d02d0b962846589a7ab5
soej24 <soej24@naver.com>
2022-05-29 23:18:38 +0900

    add

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from re import L
-from turtle import width
 import streamlit as st
 import numpy as np
 import pandas as pd
 from PIL import Image
-from app_eda import run_eda
+import altair as alt
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from app_exercise_recommendation import run_exercise_recommendationt
 from app_home import run_home
-from app_ml import run_ml
 from app_serch import run_serch
+from region_gym import run_region_gym
 
 st.set_page_config(layout="wide")
 empty1,content1,empty3=st.columns([0.1,1.2,0.1])
@@ -25,42 +27,80 @@ def main() :
         image = Image.open('data/logo.png')
         st.sidebar.image(image)  
         
-        menu = ['Home', '체육관 검색','연령별 운동추천', '지역별 국민의 체육관 참여운동', 'ML']
+        menu = ['Home', '체육관 검색','지역별 체육관 현황','지역별 국민의 체육관 참여도','연령별 운동추천']
         
         choice = st.sidebar.selectbox('main_menu', menu)
 
         if choice == menu[0] :
             run_home()
         elif choice == menu[1] :
-            run_serch()        
+            run_serch()
+
         elif choice == menu[2] :
-            run_exercise_recommendationt()
+            run_region_gym()        
+
         elif choice == menu[3] :
 
-            st.title('지역별 국민의 체육관 참여운동')
+            st.title('지역별 국민의 체육관 참여도')
 
             st.markdown("***")    
             image = Image.open('data/sub_main03_img.jpg')
             st.image(image, width=None, use_column_width=True) 
-            st.markdown("***") 
-            
-            with empty4 :
-                st.empty()
-            with col1 : 
+            st.markdown("***")           
+
+            with empty4:
+                st.empty() 
+
+            with col1 :
+                st.markdown("""
+                                <style>
+                                .css-1d391kg {
+                                 padding-top: 3.5rem;
+                                padding-right: 1rem;
+                                padding-bottom: 3.5rem;
+                                padding-left: 1rem;}
+                                </style>
+                                """, unsafe_allow_html=True)         
+                st.subheader('┏ 전체 참여 리스트 ┓') 
                 df = pd.read_csv('data/gym_use.csv',  encoding='UTF-8')
                 st.dataframe(df)
-            with col2 :
-                st.dataframe(df)
-            with empty5 :
-                st.empty()
-            
-                #run_eda()
-        
-        elif choice == menu[4] :
-            run_ml()
 
+                st.markdown("***")      
+                df_chart = df['시도명'].value_counts()
+
+                st.line_chart(df_chart)
+
+                fig1 =  px.pie(df, names='시도명', values='인구수', title='파이차트')
+                st.plotly_chart(fig1)
+
+                #plotly 의 bar 차트
+                df_sorted = df.sort_values('인구수', ascending=False)
+                fig2 = px.bar(df, x='인구수', y='시도명')
+                st.plotly_chart(fig2)
+
+
+            with col2 :
+                st.markdown("""
+                                <style>
+                                .css-1d391kg {
+                                 padding-top: 3.5rem;
+                                padding-right: 1rem;
+                                padding-bottom: 3.5rem;
+                                padding-left: 1rem;}
+                                </style>
+                                """, unsafe_allow_html=True)                      
+                st.subheader('┏ 지역별 참여 리스트 ┓')
+                df_groupby = df.groupby('시도명')['인구수'].count().sort_values(ascending = False)
+                st.table(df_groupby)
+
+            with empty5:
+                st.empty() 
+
+        elif choice == menu[4] :
+            run_exercise_recommendationt()
     with empty3:
         st.empty()
+
 
 if __name__ == '__main__' :
     main()
---
+++ region_gym.py
@@ -0,0 +1,73 @@
+from pyrsistent import v
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+import altair as alt
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+def run_region_gym() :
+
+    st.title('지역별 체육관 현황!!')
+
+    st.markdown("***")
+    image = Image.open('data/img06.jpg')
+    st.image(image, width=None, use_column_width=True)  
+
+    st.markdown("***")
+    st.markdown("""
+                <style>
+                .css-1d391kg {
+                    padding-top: 3.5rem;
+                padding-right: 1rem;
+                padding-bottom: 3.5rem;
+                padding-left: 1rem;}
+                </style>
+                """, unsafe_allow_html=True)         
+    df = pd.read_csv('data/gym_list_ch.csv')
+    df.loc[ df.groupby('소재지전체주소')['소재지전체주소'].count().sort_values(ascending = False), ]
+    # st.subheader('┏ 전체 체육관 ┓')    
+    # st.dataframe(df, height=500) 
+
+    my_order = ['서울특별시','부산광역시','대구광역시','인천광역시','광주광역시','울산광역시','세종특별자치시','경기도','강원도',\
+                '충청북도', '충청남도', '전라북도', '전라남도', '경상북도', '경상남도', '제주특별자치제도']
+    status = st.radio('지역을 선택하세요!', my_order)
+    # df.loc[ df['소재지전체주소'].str.contains('경기도'), ]
+    df = df.loc[ df['소재지전체주소'].str.contains(status), ]
+    st.markdown("***") 
+
+    # 첫번째 방법
+    if status == my_order[0] :        
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[1] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[2] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[3] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[4] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[5] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[6] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[7] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[8] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[9] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[10] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[11] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[12] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[13] :
+        st.dataframe(df.sort_values('소재지전체주소'))
+    elif status == my_order[14] :
+        st.dataframe(df.sort_values('소재지전체주소'))
